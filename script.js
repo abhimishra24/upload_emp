2f112e1c5025f0ede1a7de650ef6563de44d149d
abhimishra24 <37399397+abhimishra24@users.noreply.github.com>
2024-05-21 02:09:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+function uploadFile(){
+	const fileInput=document.getElementById('fileInput');
+	const file= fileInput.files[0];
+
+	if (!file) {
+	alert('Please select a file');
+		return;
+	}
+	
+	const formdata = new FormData();
+    //formdata.append("csvFile", fileInput.files[0], "[PROXY]");
+    //formdata.append("csvFile", fileInput.files[0]);
+	formdata.append("csvFile", file);
+	
+
+
+    const requestOptions = {
+    method: "POST",
+    body: formdata,
+    redirect: "follow"
+     };
+
+    fetch("http://localhost:5000/employees/upload", requestOptions)
+    .then((response)=> {
+		if (response.ok){
+	alert('File has been successfully uploaded');
+    return;}
+	})
+    .then((result) => console.log(result))
+    .catch((error) => console.error(error));
+	
+	
+	
+
+        
+}
+	
+
+	 
+       
+             
+
+
